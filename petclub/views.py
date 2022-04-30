baf46eb67dedb7ba9614e1ed357b3e29d5c85cef
jhoansamir <jhoansamir@DESKTOP-965MVSC.localdomain>
2022-04-29 23:49:34 -0400

    primera tarea

---
+++ petclub/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+# modulos de DRF
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+class HelloWorld(APIView):
+    def get(self, request): # verbo de la peticion como un metodo
+        # logica asociada al endpoint
+        return Response(data="Hello, World !", status=200) # respuesta del servicio
